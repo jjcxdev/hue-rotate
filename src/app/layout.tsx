da5b94f9fbaa48f42ce5c3d6cca0ae9165ba2fcd
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-10 18:59:33 -0500

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 import { Inter } from "next/font/google";
 
@@ -20,6 +21,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Hue Rotate</title>
+        <meta
+          name="description"
+          content="Hue Rotate is a React application that allows users to select an initial color and a desired color, then calculates the CSS `hue-rotate` filter value required to transition between these colors."
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://hue-rotate.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hue Rotate" />
+        <meta
+          property="og:description"
+          content="Hue Rotate is a React application that allows users to select an initial color and a desired color, then calculates the CSS `hue-rotate` filter value required to transition between these colors."
+        />
+        <meta
+          property="og:image"
+          content="https://hue-rotate.vercel.app/hue.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://hue-rotate.vercel.app/" />
+        <meta name="twitter:title" content="Hue Rotate" />
+        <meta
+          name="twitter:description"
+          content="Hue Rotate is a React application that allows users to select an initial color and a desired color, then calculates the CSS `hue-rotate` filter value required to transition between these colors."
+        />
+        <meta
+          name="twitter:image"
+          content="https://hue-rotate.vercel.app/hue.png"
+        />
+      </Head>
       <body className={`font-sans ${inter.variable}`}>{children}</body>
     </html>
   );
